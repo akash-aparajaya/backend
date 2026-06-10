8b1bafe4177312c072e07c7aa415707e6f116317
akash-aparajaya <akash@aparajayah.com>
2026-06-10 19:10:18 +0530

    incress the json size

--- src/app.js
+++ src/app.js
@@ -35,9 +35,9 @@ app.use(
 // app.use(cors());
 
 /* ---------------- BODY PARSER ---------------- */
-// app.set("trust proxy", 1);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.set("trust proxy", 1);
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 
 /* ---------------- LOGGER ---------------- */
 
